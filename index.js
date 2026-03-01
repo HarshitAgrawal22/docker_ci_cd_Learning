5bf503b042ff145e6058559a13fac2f8288678b1
HarshitAgrawal22 <haharshitagrawal22@gmail.com>
2026-03-01 22:45:16 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+
+const app = express();
+
+
+app.use("/", (req, res) => {
+    return res.json({
+        message: "hello brother"
+    })
+});
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => console.log(`started server and running on ${port}`));
